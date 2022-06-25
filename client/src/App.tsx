70e9d4b66ce2ac2775b3a74470291870433ad3a1
Guilherme Vinicius <guilherme@fieldcontrol.com.br>
2022-06-25 14:56:31 -0300

    chore: initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="stroke-neutral-600">ZIPZOP APP</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
